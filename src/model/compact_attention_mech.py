f85f6da70823c0b3f961982264ed7affd1fd333d
Daro Arteaga <dariojose22@outlook.com>
2025-10-16 21:58:34 -0500

    Updated name for selfattention class v1 and v2

--- src/model/compact_attention_mech.py
+++ src/model/compact_attention_mech.py
@@ -22,8 +22,23 @@ class CompactAttentionMech(nn.Module):
 
         context_vec = attn_weights @ values 
         return context_vec
+class CompactAttentionMechV2(nn.Module):
 
-inputs = torch.rand(2, 3)
-sa_v1 = CompactAttentionMech(3,3)
 
-print(sa_v1(inputs))
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        
+    def forward(self, x):
+        keys = x @ self.W_key
+        querys = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = querys @ keys.T 
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values 
+        return context_vec
